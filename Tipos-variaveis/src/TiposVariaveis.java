5d64459cdf72977b205cd3b902b844d49dbdfaa7
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-08-28 18:24:53 -0300

    exercicio simples de logica ainda não completo

---
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception { 
+        //int numero = 5;  
+
+       // numero++;
+         
+       // System.out.println(numero ); 
+        int numero1 = 1; 
+        int numero2 = 2; 
+        
+        boolean simNao = numero1 == numero2;        
+
+        
+    }
+}
