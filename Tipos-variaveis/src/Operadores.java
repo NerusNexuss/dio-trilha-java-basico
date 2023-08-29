02f34cd34b66e9f0ee3cc83b06b60e5a9f892084
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-08-29 13:16:29 -0300

    estudos

---
+++ Tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+  boolean condicao1 = true; 
+
+  boolean condicao2 = true; 
+
+  if(condicao1 && (7 > 4)){
+    System.out.println("as duas condiçoes são verdadeiras");
+  } 
+
+  if(condicao1 || condicao2){
+      System.out.println("as duas condições são verdadeiras");
+  }
+}
--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -8,7 +8,13 @@ public class TiposVariaveis {
         int numero1 = 1; 
         int numero2 = 2; 
         
-        boolean simNao = numero1 == numero2;
-        
+        boolean simNao = numero1 == numero2; 
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao); 
+
+        simNao = numero1 > numero2; 
+
+        System.out.println("numeroUm é diferente a numeroDois?" + simNao);
+
     }
 }
