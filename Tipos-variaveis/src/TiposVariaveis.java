815a6a851b53100cf50223e9a59cfd1520b304d5
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-08-29 11:02:40 -0300

    teste

--- Tipos-variaveis/src/TiposVariaveis.java
+++ Tipos-variaveis/src/TiposVariaveis.java
@@ -8,8 +8,7 @@ public class TiposVariaveis {
         int numero1 = 1; 
         int numero2 = 2; 
         
-        boolean simNao = numero1 == numero2;        
+        boolean simNao = numero1 == numero2;
 
-        
     }
 }
